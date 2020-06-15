d11ffba39b887965dc897ee4294323828d46d281
Aliaksandr Terashkou <aliaksandr.terashkou@itechart-group.com>
2020-06-15 18:12:03 +0300

    Initial Commit

---
+++ mvvm-base/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  mvvm-base
+//
+//  Created by nb-058-41b on 6/15/20.
+//  Copyright © 2020 Alexander Tereshkov. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
